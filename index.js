eecca7e14edc6a0a323d7dda4bacb7f39e4c4cd7
ARlMASU <onurefekocak68@gmail.com>
2024-05-28 00:50:12 +0200

    commitaaaaaaa

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const features = document.querySelector('#features');
-const story = document.querySelector('#story');
-const navLogoCircle = document.querySelector('.nav-logo-circle');
+const features = document.querySelector("#features");
+const story = document.querySelector("#story");
+const navLogoCircle = document.querySelector(".nav-logo-circle");
 // const footerLogo = document.querySelector('.footer-logo');
 // const footerCentralPart = document.querySelector('.footer-central-part');
 // const footerButton = document.querySelector('.play-now-button-footer');
@@ -13,24 +13,27 @@ const navLogoCircle = document.querySelector('.nav-logo-circle');
 // footerButton.style.paddingLeft = Math.round(widthCentralPart) * 2 + "px";
 
 function scrollToElement(element) {
-  if(element != home){
+  if (element != home) {
     window.scrollTo({
-      top: Math.round(element.getBoundingClientRect().top + document.documentElement.scrollTop) - 100,
-      behavior: 'smooth',
-    })
-  }
-  else{
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      })
+      top:
+        Math.round(
+          element.getBoundingClientRect().top +
+            document.documentElement.scrollTop
+        ) - 100,
+      behavior: "smooth",
+    });
+  } else {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   }
 }
 
 let velocity = 0;
 let velocityDeg = 0;
 
-function animatetesmorts(){
+function animatetesmorts() {
   velocity += 100;
 
   velocityDeg = velocity + "deg";
